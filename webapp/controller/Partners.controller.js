5540fcc8b7cd15e8a4861846477d3c49ca6c9434
Fernando Freire <fernando.freire@lab2dev.com>
2024-03-08 13:26:42 +0000

    feat smart table

--- webapp/controller/Partners.controller.js
+++ webapp/controller/Partners.controller.js
@@ -1,15 +1,28 @@
 sap.ui.define([
     "com/lab2dev/citrosuco/controller/BaseController",
+    "sap/ui/model/json/JSONModel",
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("com.lab2dev.citrosuco.controller.Partners", {
             onInit: function () {
+                this._setMockDataModel();
+            },
 
+            _setMockDataModel: async function () {
+                const oModel = new JSONModel();
+                const oComponent = this.getOwnerComponent();
+                const resolveRefText = oComponent.getManifestObject().resolveUri("model/mockData.json");
+                oModel.loadData(resolveRefText);
+                this.getView().setModel(oModel, "mockData");
+            },
+
+            test: function () {
+                console.log("oi")
             }
         });
     });
