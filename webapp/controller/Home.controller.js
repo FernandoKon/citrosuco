1039816f344157ec1eb51c46bfb952d2368da9e7
Fernando Freire <fernando.freire@lab2dev.com>
2024-03-11 20:21:26 +0000

    rafactor smart Table

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -10,6 +10,16 @@ sap.ui.define([
         return Controller.extend("com.lab2dev.citrosuco.controller.Home", {
             onInit: function () {
 
+            },
+
+            handleSelectionChange: function (oEvent) {
+                const oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                const oSelectedItem = oEvent.getSource();
+                const sID = oSelectedItem.getBindingContext().getProperty("ID");
+
+                oRouter.navTo("SupplierDetail", {
+                    supplierID: sID
+                });
             }
         });
     });
