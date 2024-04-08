fbffd836c7469cf93ba89df6d338e3322c75f448
Fernando Kon <fernando.freire@lab2dev.com>
2024-04-08 13:50:20 +0000

    refactor views

--- webapp/controller/PortionDetail.controller.js
+++ webapp/controller/PortionDetail.controller.js
@@ -7,18 +7,17 @@ sap.ui.define([
     function (Controller) {
         "use strict";
 
-        return Controller.extend("com.lab2dev.citrosuco.controller.EventDetail", {
+        return Controller.extend("com.lab2dev.citrosuco.controller.PortionDetail", {
             onInit: async function () {
                 const oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-                await oRouter.getRoute("EventDetail").attachMatched(this._onRouteMatched, this);
+                await oRouter.getRoute("PortionDetail").attachMatched(this._onRouteMatched, this);
 
             },
 
             _onRouteMatched: function (oEvent) {
-                debugger
                 const params = oEvent.getParameter("arguments")
                 
-                let bindString = "/EvtContrEntrGet(";
+                let bindString = "/EvtParcGet(";
 
                 Object.keys(params).forEach((key, index) => {
                     if (index > 0) {
