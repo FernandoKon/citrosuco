54c4fe5fc229d14a734eb30a280ea83f5c8f3dc3
Fernando Kon <fernando.freire@lab2dev.com>
2024-06-17 14:15:30 +0000

    feat fruit processing macro plan

---
+++ webapp/controller/Fruit.controller.js
@@ -0,0 +1,16 @@
+sap.ui.define([
+    "com/lab2dev/citrosuco/controller/BaseController",
+    "com/lab2dev/citrosuco/utilities/utilities"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller, utilities) {
+        "use strict";
+
+        return Controller.extend("com.lab2dev.citrosuco.controller.Fruit", {
+            onInit: function () {
+                
+            },
+        });
+    });
