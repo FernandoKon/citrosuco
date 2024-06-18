bde427849f8861bf6be45e421b1c7cfb534479b5
Fernando Kon <fernando.freire@lab2dev.com>
2024-06-18 18:21:26 +0000

    new visions

---
+++ webapp/controller/FruitCadence.controller.js
@@ -0,0 +1,18 @@
+sap.ui.define([
+    "com/lab2dev/citrosuco/controller/BaseController",
+    "sap/ui/model/json/JSONModel",
+    "com/lab2dev/citrosuco/utilities/utilities"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller, JSONModel, utilities) {
+        "use strict";
+
+        return Controller.extend("com.lab2dev.citrosuco.controller.FruitCadence", {
+            onInit: function () {
+                
+            },
+
+        });
+    });
